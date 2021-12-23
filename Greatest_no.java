848aa6d9787739bdefc19d51647660c2b06060db
Quamran <96306767+Quamran@users.noreply.github.com>
2021-12-23 17:44:53 +0530

    Add files via upload

---
+++ Greatest_no.java
@@ -0,0 +1,28 @@
+package Geekster;
+
+public class Greatest_no {
+	
+	public static void main(String[] args) {
+
+        
+		int a = 45 ,  b = 56 , c = 13 ;
+		
+		if(a > b && a > c) 
+			
+			System.out.println( a + "  is greatest number");
+		
+		else if (b > a && b > c)
+			
+			System.out.println( b +"  is greatest number");
+		
+		else
+			System.out.println(c + "  is greatest number");
+		
+			
+			
+		}
+		
+		
+	}
+
+
